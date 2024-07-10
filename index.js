5a9969c202cd9a09d86b98729b59b9ce9888899c
joelwalice <joelwalice@gmail.com>
2024-07-10 09:48:53 +0530

    FIX

--- index.js
+++ index.js
@@ -10,7 +10,11 @@ const PORT = process.env.PORT || 1337;
 
 expressApp.use(express.json());
 
-expressApp.use(cors());
+expressApp.use(cors({
+  origin: 'https://newdashboard.re4billion.ai',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+}));
 
 expressApp.use('/', require('./routes/userRoutes'));
 
